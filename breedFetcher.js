e6a9ffcec7b2dfe8c04cc8eb4166772074d1a527
Kelsey Erickson <kerickson@gmx.com>
2021-08-20 19:44:58 +0000

    Modularized code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,35 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
 
-  if (error !== null) {
 
-    console.log(error);
-    process.exit();
+const fetchBreedDescription = function (breedname, callback) {
 
-  }
-  
-  const data = JSON.parse(body);
- 
-  
-  if (data[0] === undefined) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedname}`, (error, response, body) => {
 
-    console.log('Request not Found');
-    process.exit();
+    if (error !== null) {
 
-  }
- 
-  console.log(data[0].description);
-  
+      callback(error, null);
+      process.exit();
 
-    
-    
+    }
 
-});
+    const data = JSON.parse(body);
+
+
+    if (data.length === 0) {
+
+      callback('Request not Found', null);
+      process.exit();
+
+    }
+
+    callback(null, data[0].description);
+
+
+
+  });
+
+
+};
+
+module.exports = { fetchBreedDescription };
